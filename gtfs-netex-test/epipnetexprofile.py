c5775008a7a3c9d97c52ce526640adf12aaa0877
Stefan de Konink <stefan@konink.de>
2023-11-28 11:06:44 +0100

    Might be needed

---
+++ gtfs-netex-test/epipnetexprofile.py
@@ -0,0 +1,146 @@
+import datetime
+from typing import List
+
+from xsdata.formats.dataclass.serializers import XmlSerializer
+from xsdata.formats.dataclass.serializers.config import SerializerConfig
+from xsdata.models.datatype import XmlDateTime
+
+from netex import PublicationDelivery, DataObjectsRelStructure, CompositeFrame, CodespacesRelStructure, \
+    VersionsRelStructure, FramesRelStructure, Codespace, ServiceFrame, TypeOfFrame, TypeOfFrameRef, ResourceFrame, \
+    DataSourcesInFrameRelStructure, OrganisationsInFrameRelStructure, OperationalContextsInFrameRelStructure, \
+    TimetableFrame, LinesInFrameRelStructure, ScheduledStopPointsInFrameRelStructure, JourneysInFrameRelStructure, \
+    JourneyPatternsInFrameRelStructure, RoutesInFrameRelStructure, RoutePointsInFrameRelStructure, \
+    RouteLinksInFrameRelStructure, DataManagedObjectStructure, Line, TransportOrganisationRefsRelStructure, ServiceLink
+from refs import getId
+# from src.refs import setIdVersion
+from timetabledpassingtimesprofile import TimetablePassingTimesProfile
+
+
+class EPIPNeTexProfile(TimetablePassingTimesProfile):
+    codespace: Codespace
+
+    @staticmethod
+    def clean_element(element: DataManagedObjectStructure):
+        element.derived_from_object_ref = None
+        element.version = None
+        element.data_source_ref_attribute = None
+
+    # def getServiceLinks(self) -> List[ServiceLink]:
+    #     for x in self.timing_links:
+
+
+    def getResourceFrame(self) -> ResourceFrame:
+        resource_frame = ResourceFrame(id=getId(ResourceFrame, self.codespace, "ResourceFrame"), version=self.version.version)
+        # resource_frame.data_sources = DataSourcesInFrameRelStructure(data_source=[self.data_source])
+        # TODO: Zone information can be obtained from stops.txt
+        # resource_frame.zones = ZonesInFrameRelStructure(transport_administrative_zone=[transport_administrative_zone])
+        # resource_frame.organisations = OrganisationsInFrameRelStructure(operator=self.operators)
+        # resource_frame.operational_contexts = OperationalContextsInFrameRelStructure(
+        #     operational_context=self.getOperationalContexts())
+        # resource_frame.vehicle_types = VehicleTypesInFrameRelStructure(compound_train_or_train_or_vehicle_type=getVehicleTypes(codespace))
+        # resource_frame.vehicles = VehiclesInFrameRelStructure(train_element_or_vehicle=getVehicles(codespace))
+        return resource_frame
+
+    def getTimetableFrame(self, clean=False) -> TimetableFrame:
+        self.getTimetabledPassingTimes(force=False, clean=clean)
+
+        if clean:
+            for element in self.service_journeys:
+                EPIPNeTexProfile.clean_element(element)
+
+        timetable_frame = TimetableFrame(id = "TimetableFrame")
+        timetable_frame.vehicle_journeys = JourneysInFrameRelStructure(choice=self.service_journeys)
+        return timetable_frame
+
+    @staticmethod
+    def clean_line(line: Line):
+        if line.operator_ref and line.authority_ref:
+            line.additional_operators = TransportOrganisationRefsRelStructure(operator_ref=[line.operator_ref])
+            line.operator_ref = None
+
+    def getServiceFrame(self, clean=False) -> ServiceFrame:
+        if clean:
+            for element in self.lines:
+                EPIPNeTexProfile.clean_line(element)
+                EPIPNeTexProfile.clean_element(element)
+            for element in self.flexible_lines:
+                EPIPNeTexProfile.clean_element(element)
+            for element in self.scheduled_stop_points:
+                EPIPNeTexProfile.clean_element(element)
+            for element in self.service_journey_patterns:
+                element.route_view = None
+                element.direction_type = None
+                EPIPNeTexProfile.clean_element(element)
+                for stop_point_in_journey_pattern in element.points_in_sequence.point_in_journey_pattern_or_stop_point_in_journey_pattern_or_timing_point_in_journey_pattern:
+                    EPIPNeTexProfile.clean_element(stop_point_in_journey_pattern)
+            for element in self.routes:
+                EPIPNeTexProfile.clean_element(element)
+                for route_point in element.points_in_sequence.point_on_route:
+                    EPIPNeTexProfile.clean_element(route_point)
+            for element in self.route_points:
+                EPIPNeTexProfile.clean_element(element)
+            for element in self.route_links:
+                EPIPNeTexProfile.clean_element(element)
+
+        service_frame = ServiceFrame(id = "ServiceFrame")
+        service_frame.type_of_frame_ref = TypeOfFrameRef(ref="EPIP:EU_PI_NETWORK")
+        service_frame.lines = LinesInFrameRelStructure(flexible_line_or_line=(self.lines + self.flexible_lines))
+
+        # TODO service_frame.destination_displays
+
+        service_frame.scheduled_stop_points = ScheduledStopPointsInFrameRelStructure(scheduled_stop_point=self.scheduled_stop_points)
+
+        # TODO service_frame.service_links =
+        # TODO service_frame.connections =
+        # TODO service_frame.stop_assignments =
+
+        service_frame.journey_patterns = JourneyPatternsInFrameRelStructure(choice=self.service_journey_patterns)
+
+        # Leesbaarheid
+        # service_frame.routes = RoutesInFrameRelStructure(route=self.routes)
+        # service_frame.route_points = RoutePointsInFrameRelStructure(route_point=self.route_points)
+        # if len(self.route_links) > 0:
+        #     service_frame.route_links = RouteLinksInFrameRelStructure(route_links=self.route_links)
+
+        return service_frame
+
+    def getCompositeFrame(self) -> CompositeFrame:
+        composite_frame = CompositeFrame(id=getId(CompositeFrame, self.codespace, self.data_source.short_name.value), version=self.version.version)
+        composite_frame.type_of_frame_ref = TypeOfFrameRef(ref="EPIP:EU_PI_NETWORK_OFFER")
+        composite_frame.frame_defaults = self.frame_defaults
+        composite_frame.codespaces = CodespacesRelStructure(codespace_ref_or_codespace=[self.codespace])
+        composite_frame.versions = None
+
+        # ServiceJourneyPatterns may be computed here:
+        timetable_frame = self.getTimetableFrame(clean=True)
+        service_frame = self.getServiceFrame(clean=True)
+
+        composite_frame.frames = FramesRelStructure(resource_frame=[self.getResourceFrame()], service_frame=[service_frame], timetable_frame=[timetable_frame])
+
+        return composite_frame
+
+    def getPublicationDelivery(self) -> PublicationDelivery:
+        composite_frame = self.getCompositeFrame()
+
+        publication_delivery = PublicationDelivery(
+            publication_timestamp=XmlDateTime.from_datetime(datetime.datetime.now()))
+        publication_delivery.version = "ntx:1.1"
+        publication_delivery.participant_ref = "NDOV"
+        publication_delivery.description = "NeTEx export"
+        publication_delivery.data_objects = DataObjectsRelStructure(choice=[composite_frame])
+
+        return publication_delivery
+
+    def __init__(self):
+        pass
+        # self.codespace, self.data_source, self.version, self.frame_defaults = self.getCodespaceAndDataSource()
+        # self.operators = self.getOperators()
+        # self.lines = self.getLines()
+        # self.routes, self.route_points, self.route_links = self.getRoutes()
+        # self.stop_areas = self.getStopAreas()
+        # self.scheduled_stop_points = self.getScheduledStopPoints()
+        # self.availability_conditions = self.getAvailabilityConditions()
+        # self.service_journeys = self.getServiceJourneys()
+        # self.service_journey_patterns, self.timing_links = self.getServiceJourneyPatterns()
+        # self.time_demand_types = self.getTimeDemandTypes()
+
---
+++ gtfs-netex-test/timetabledpassingtimesprofile.py
@@ -0,0 +1,132 @@
+from netex import ServiceJourney, ServiceJourneyPattern, StopPointInJourneyPattern, TimetabledPassingTime, \
+    PointsInJourneyPatternRelStructure, Codespace, TimetabledPassingTimesRelStructure, \
+    PointInJourneyPatternRef, ServiceJourneyPatternRef, Call, MultilingualString, RouteView
+from refs import getRef, getIndex, getId
+import sys
+
+class TimetablePassingTimesProfile:
+    codespace: Codespace
+    service_journeys: list[ServiceJourney]
+    def __init__(self, codespace, version, service_journeys, service_journey_patterns):
+        self.codespace = codespace
+        # self.version = version
+        self.service_journeys = service_journeys
+        self.service_journey_patterns = service_journey_patterns
+
+    @staticmethod
+    def mapCallToStopPointInJourneyPattern(call: Call) -> StopPointInJourneyPattern:
+        stop_point_in_journey_pattern = StopPointInJourneyPattern(
+            id=call.id.replace(":Call:", ":StopPointInJourneyPattern:"),
+            version=call.version,
+            order=call.order,
+            derived_from_object_ref=call.id,
+            derived_from_version_ref_attribute=call.version
+        )
+        stop_point_in_journey_pattern.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref = call.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref_or_scheduled_stop_point_view
+        stop_point_in_journey_pattern.for_alighting = call.arrival.for_alighting
+        stop_point_in_journey_pattern.for_boarding = call.departure.for_boarding
+        stop_point_in_journey_pattern.onward_timing_link_ref = call.onward_timing_link_view
+        stop_point_in_journey_pattern.destination_display_ref_or_destination_display_view = call.destination_display_ref_or_destination_display_view
+        stop_point_in_journey_pattern.onward_service_link_ref = call.onward_service_link_ref_or_onward_service_link_view  # TODO: select only the service_link_ref
+        return stop_point_in_journey_pattern
+
+    @staticmethod
+    def mapCallToTimetabledPassingTime(call: Call, pattern: dict) -> TimetabledPassingTime:
+        timetabled_passing_time = TimetabledPassingTime(id=call.id.replace(":Call:", ":TimetabledPassingTime:"),
+                                                        version=call.version,
+                                                        derived_from_object_ref=call.id,
+                                                        derived_from_version_ref_attribute=call.version
+                                                        )
+        timetabled_passing_time.choice_1 = getRef(pattern[call.order])
+        timetabled_passing_time.derived_from_object_ref = call.id
+        timetabled_passing_time.derived_from_version_ref_attribute = call.version
+        timetabled_passing_time.arrival_time = call.arrival.time
+        timetabled_passing_time.arrival_day_offset = call.arrival.day_offset
+        timetabled_passing_time.departure_time = call.departure.time
+        timetabled_passing_time.departure_day_offset = call.departure.day_offset
+        return timetabled_passing_time
+
+    def getTimetabledPassingTimes(self, force=False, clean=False):
+        sjps = {}
+        existing_sjps = getIndex(self.service_journey_patterns)
+
+        sj: ServiceJourney
+        for sj in self.service_journeys:
+            if sj.passing_times and not force:
+                if clean:
+                    sj.calls = None
+                    sj.wait_times = None
+                    sj.run_times = None
+                    sj.time_demand_type_ref = None
+                    sj.time_demand_types = None
+                    sj.validity_conditions_or_valid_between = None
+
+                continue
+
+            # if there are calls -> create service journey patterns
+            elif sj.calls:
+                service_journey_pattern: ServiceJourneyPattern
+                service_journey_pattern = None
+                if not sj.choice:
+
+                    call: Call
+                    spijps = [self.mapCallToStopPointInJourneyPattern(call) for call in sj.calls.choice]
+
+                    spijp_hash = str(hash(
+                        tuple([(spijp.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref.ref, spijp.for_alighting, spijp.for_boarding,
+                                spijp.onward_timing_link_ref, spijp.onward_service_link_ref,
+                                spijp.destination_display_ref_or_destination_display_view)
+                               for spijp in spijps])))
+
+                    service_journey_pattern = ServiceJourneyPattern(id=getId(ServiceJourneyPattern, self.codespace, spijp_hash),
+                                                                    version=sj.version,
+                                                                    route_ref_or_route_view=RouteView(flexible_line_ref_or_line_ref_or_line_view=sj.choice_1),
+                                                                    name=MultilingualString(value=spijp_hash),
+                                                                    derived_from_object_ref = sj.id,
+                                                                    derived_from_version_ref_attribute = sj.version)
+
+                    if len(spijps) > 0:
+                        service_journey_pattern.points_in_sequence = PointsInJourneyPatternRelStructure(point_in_journey_pattern_or_stop_point_in_journey_pattern_or_timing_point_in_journey_pattern=spijps)
+
+                    sjps[spijp_hash] = service_journey_pattern
+                    sj.choice = getRef(service_journey_pattern, ServiceJourneyPatternRef)
+                    existing_sjps[service_journey_pattern.id] = service_journey_pattern
+
+                if not service_journey_pattern:
+                    service_journey_pattern = existing_sjps[sj.service_journey_pattern_ref.ref]
+
+                pattern = {x.order: x for x in service_journey_pattern.points_in_sequence.point_in_journey_pattern_or_stop_point_in_journey_pattern_or_timing_point_in_journey_pattern}
+
+                ttpt = []
+                for call in sj.calls.choice:
+                    # TODO: do something with the different elements of the choice (Call, CallZ, DatedCall, DatedCallZ)
+                    pis = pattern[call.order]
+                    if pis.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref.ref != call.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref_or_scheduled_stop_point_view.ref: # TODO: make sure we get the right one
+                        print("{} order does not match {} order ({} vs {})".format(service_journey_pattern.id, sj.id,
+                                                                                   call.scheduled_stop_point_ref,
+                                                                                   pis.scheduled_stop_point_ref),
+                              file=sys.stderr)
+
+                    else:
+                        ttpt.append(self.mapCallToTimetabledPassingTime(call, pattern))
+
+                if len(ttpt) > 0:
+                    sj.passing_times = TimetabledPassingTimesRelStructure(timetabled_passing_time=ttpt)
+
+            # if there is a servicejourneypattern, departure time, timedemandtype it can be expanded
+            elif sj.service_journey_pattern_ref and sj.departure_time and sj.time_demand_type_ref:
+                pass
+
+            # if there are servicejourneypattern, departure time, waittimes, runtimes they can be expanded
+            elif sj.service_journey_pattern_ref and sj.departure_time and sj.run_times:
+                pass
+
+            if clean:
+                sj.calls = None
+                sj.wait_times = None
+                sj.run_times = None
+                sj.time_demand_type_ref = None
+                sj.time_demand_types = None
+                sj.validity_conditions_or_valid_between = None
+
+        self.service_journey_patterns += sjps.values()
