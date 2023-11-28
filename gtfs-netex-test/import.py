0ca6cd9adce7a639edce79023842497a1647207f
Stefan de Konink <stefan@konink.de>
2023-11-28 11:25:12 +0100

    Clean import every time

--- gtfs-netex-test/import.py
+++ gtfs-netex-test/import.py
@@ -2,6 +2,17 @@ import duckdb
 
 con = duckdb.connect(database='gtfs2.duckdb')
 with duckdb.cursor(con) as cur:
+    cur.execute("""DROP TABLE IF EXISTS feed_info;""")
+    cur.execute("""DROP TABLE IF EXISTS agency;""")
+    cur.execute("""DROP TABLE IF EXISTS calendar_dates;""")
+    cur.execute("""DROP TABLE IF EXISTS calendar;""")
+    cur.execute("""DROP TABLE IF EXISTS routes;""")
+    cur.execute("""DROP TABLE IF EXISTS stops;""")
+    cur.execute("""DROP TABLE IF EXISTS shapes;""")
+    cur.execute("""DROP TABLE IF EXISTS trips;""")
+    cur.execute("""DROP TABLE IF EXISTS transfers;""")
+    cur.execute("""DROP TABLE IF EXISTS stop_times;""")
+
     cur.execute("""CREATE TABLE feed_info AS SELECT * FROM read_csv('gtfs/feed_info.txt', delim=',', header=true, auto_detect=true);""")
     cur.execute("""CREATE TABLE agency AS SELECT * FROM read_csv('gtfs/agency.txt', delim=',', header=true, auto_detect=true);""")
     cur.execute("""CREATE TABLE calendar_dates AS SELECT * FROM read_csv('gtfs/calendar_dates.txt', delim=',', header=true, auto_detect=true);""")
