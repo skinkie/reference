6a0c826be6e6eadefb75b7bc17683584564d1803
Stefan de Konink <stefan@konink.de>
2023-12-10 01:51:28 +0100

    Introduce ServiceJourneyPattern to Route inference

--- gtfs-netex-test/anytodutch.py
+++ gtfs-netex-test/anytodutch.py
@@ -18,7 +18,9 @@ from netex import Codespace, DataSource, ResponsibilitySet, Version, Operator, O
     TransportAdministrativeZone, MultilingualString, RoutePoint, RouteLink, Route, Line, DestinationDisplay, \
     ScheduledStopPoint, StopArea, PassengerStopAssignment, TimingPoint, TimingLink, ServiceJourneyPattern, \
     TimeDemandType, Notice, NoticeAssignment, AvailabilityCondition, OperatorView, Authority, ServiceJourney, \
-    ServiceCalendar, PrivateCode, ServiceLink, LocationStructure2, Pos
+    ServiceCalendar, PrivateCode, ServiceLink, LocationStructure2, Pos, AllVehicleModesOfTransportEnumeration, \
+    LineRefStructure, DirectionType, PointsOnRouteRelStructure, StopPointInJourneyPattern, TimingPointInJourneyPattern, \
+    PointProjection, PointRefStructure, PointOnRoute, RouteView, ProjectionsRelStructure, RoutePointRefStructure
 from refs import getId, getRef
 from timedemandtypesprofile import TimeDemandTypesProfile
 
@@ -28,6 +30,8 @@ def parseFromEtree(parser, tree, element, clazz) -> List:
     return [parser.parse(element, clazz) for element in tree.iterfind(f".//{element}")]
 
 def convertReprojectLocation(location: LocationStructure2, transformer: Transformer):
+    # TODO: implement a complete naive method, that reads the srs values, instead of hardcoding them.
+
     if transformer.target_crs == '4326':
         if location.latitude is not None and location.longitude is not None:
             return
@@ -78,6 +82,129 @@ def convertReprojectLocation(location: LocationStructure2, transformer: Transfor
                 location.pos = Pos(value=[x, y], srs_dimension=2, srs_name=target_srs_name)
 
 
+def mapPointToRoutePoint(point_ref, ssps: Dict[str, ScheduledStopPoint], tps: Dict[str, ScheduledStopPoint], rps: Dict[str, RoutePoint]):
+    if point_ref in ssps:
+        point = ssps[point_ref]
+    else:
+        point = tps[point_ref]
+
+    rp: RoutePoint = TimeDemandTypesProfile.getObjectFromObject(point, RoutePoint,
+                                                                point.id.replace(point.__class__.__name__, 'RoutePoint'))
+    rps[point.id] = rp
+    if point.projections is None:
+        point.projections = ProjectionsRelStructure(
+            choice=[PointProjection(id=point.id.replace(point.__class__.__name__, 'PointProjection'),
+                                    version=point.version, project_to_point_ref=getRef(rp, PointRefStructure))])
+
+    return rp
+
+def mapPointInJourneyPattern(x, ssps: Dict[str, ScheduledStopPoint],
+                             tps: Dict[str, TimingPoint],
+                             tls: Dict[str, TimingLink],
+                             sls: Dict[str, ServiceLink],
+                             rps: Dict[str, RoutePoint],
+                             rls: Dict[str, RouteLink]
+                             ):
+    por: PointOnRoute
+    rl: RouteLink = None
+
+    if isinstance(x, StopPointInJourneyPattern):
+        x: StopPointInJourneyPattern = x
+        ssp_ref = x.fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref.ref
+        rp = rps.get(ssp_ref, None)
+        if rp is None:
+            rp = mapPointToRoutePoint(ssp_ref, ssps, tls, rps)
+
+        if x.onward_service_link_ref is not None:
+            sl_ref = x.onward_service_link_ref.ref
+            rl = rls.get(sl_ref, None)
+            if rl is None:
+                sl = sls[sl_ref]
+                from_rp = rps.get(sl.from_point_ref.ref, None)
+                if from_rp is None:
+                    from_rp = mapPointToRoutePoint(sl.from_point_ref.ref, ssps, tps, rps)
+
+                to_rp = rps.get(sl.to_point_ref.ref, None)
+                if to_rp is None:
+                    to_rp = mapPointToRoutePoint(sl.to_point_ref.ref, ssps, tps, rps)
+
+                rl: RouteLink = TimeDemandTypesProfile.getObjectFromObject(sl, RouteLink, sl.id.replace('ServiceLink', 'RouteLink'))
+                rl.from_point_ref = getRef(from_rp, RoutePointRefStructure)
+                rl.to_point_ref = getRef(to_rp, RoutePointRefStructure)
+                rls[sl_ref] = rl
+
+        elif x.onward_timing_link_ref is not None:
+            tl_ref = x.onward_timing_link_ref.ref
+            rl = rls.get(tl_ref, None)
+            if rl is None:
+                tl = tls[tl_ref]
+                # TODO: Do this in the academic correct way, using nameOfRefClass
+                from_rp = rps.get(tl.from_point_ref.ref, None)
+                if from_rp is None:
+                    from_rp = mapPointToRoutePoint(tl.from_point_ref.ref, ssps, tps, rps)
+
+                to_rp = rps.get(tl.to_point_ref.ref, None)
+                if to_rp is None:
+                    to_rp = mapPointToRoutePoint(tl.to_point_ref.ref, ssps, tps, rps)
+
+                rl: RouteLink = TimeDemandTypesProfile.getObjectFromObject(tl, RouteLink, tl.id.replace('TimingLink', 'RouteLink'))
+                rl.from_point_ref = getRef(from_rp, RoutePointRefStructure)
+                rl.to_point_ref = getRef(to_rp, RoutePointRefStructure)
+                rls[tl_ref] = rl
+
+    elif isinstance(x, TimingPointInJourneyPattern):
+        x: TimingPointInJourneyPattern = x
+        tp_ref = x.choice_1.ref
+        rp = rps.get(tp_ref, None)
+        if rp is None:
+            tp = tps[tp_ref]
+            rp: RoutePoint = TimeDemandTypesProfile.getObjectFromObject(tp, RoutePoint, tp.id.replace('TimingPoint', 'RoutePoint'))
+            rps[tp_ref] = rp
+            if tp.projections is None:
+                tp.projections = ProjectionsRelStructure(choice=[PointProjection(id=tp.id.replace('TimingPoint', 'PointProjection'), version=tp.version, project_to_point_ref=getRef(rp, PointRefStructure))])
+
+        if x.onward_timing_link_ref is not None:
+            tl_ref = x.onward_timing_link_ref.ref
+            rl = rls.get(tl_ref, None)
+            if rl is None:
+                tl = tls[tl_ref]
+                from_rp = rps.get(tl.from_point_ref.ref, None)
+                if from_rp is None:
+                    from_rp = mapPointToRoutePoint(tl.from_point_ref.ref, ssps, tps, rps)
+
+                to_rp = rps.get(tl.to_point_ref.ref, None)
+                if to_rp is None:
+                    to_rp = mapPointToRoutePoint(tl.to_point_ref.ref, ssps, tps, rps)
+
+                rl: RouteLink = TimeDemandTypesProfile.getObjectFromObject(tl, RouteLink, tl.id.replace('TimingLink', 'RouteLink'))
+                rl.from_point_ref = getRef(from_rp, RoutePointRefStructure)
+                rl.to_point_ref = getRef(to_rp, RoutePointRefStructure)
+                rls[tl_ref] = rl
+
+    por: PointOnRoute = TimeDemandTypesProfile.getObjectFromObject(x, PointOnRoute, x.id.replace('StopPointInJourneyPattern', 'PointOnRoute'))
+    por.choice_1 = getRef(rp)
+    if rl is not None:
+        por.onward_route_link_ref = getRef(rl)
+    return por
+
+def routeFromServiceJourneyPattern(service_journey_pattern: ServiceJourneyPattern,
+                                     ssps: Dict[str, ScheduledStopPoint],
+                                     tps: Dict[str, TimingPoint],
+                                     tls: Dict[str, TimingLink],
+                                     sls: Dict[str, ServiceLink],
+                                     rps: Dict[str, RoutePoint],
+                                     rls: Dict[str, RouteLink]
+                                   ) -> Route:
+    # Idea: Copy over ServiceJourneyPattern
+    route = TimeDemandTypesProfile.getObjectFromObject(service_journey_pattern, Route, service_journey_pattern.id.replace('ServiceJourneyPattern', 'Route'))
+    if isinstance(service_journey_pattern.route_ref_or_route_view, RouteView):
+        # TODO: alle attributen kopieren
+        route.flexible_line_ref_or_line_ref = service_journey_pattern.route_ref_or_route_view.flexible_line_ref_or_line_ref_or_line_view
+
+    route.points_in_sequence = PointsOnRouteRelStructure(point_on_route=[mapPointInJourneyPattern(x, ssps, tps, tls, sls, rps, rls)
+                                                                         for x in service_journey_pattern.points_in_sequence.point_in_journey_pattern_or_stop_point_in_journey_pattern_or_timing_point_in_journey_pattern])
+
+    return route
 
 import datetime
 from pytz import timezone
@@ -135,6 +262,7 @@ def conversion(input_filename: str, output_filename: str):
 
     operational_contexts: List[OperationalContext] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}OperatinalContext", OperationalContext)
 
+
     vehicle_types: List[VehicleType] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}VehicleType", VehicleType)
 
     transport_administrative_zones: List[TransportAdministrativeZone] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}TransportAdministrativeZone", TransportAdministrativeZone)
@@ -151,6 +279,12 @@ def conversion(input_filename: str, output_filename: str):
     routes: List[Route] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}Route", Route)
 
     lines: List[Line] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}Line", Line)
+    if len(lines):
+        # TODO: Introduce a single operator line?
+        pass
+
+    if len(operational_contexts) == 0:
+        operational_contexts += [OperationalContext(id=getId(OperationalContext, codespaces[0], str(tm)), version=versions[0].version, vehicle_mode=AllVehicleModesOfTransportEnumeration(tm)) for tm in set([line.transport_mode for line in lines])]
 
     destination_displays: List[DestinationDisplay] = parseFromEtree(parser, tree, "{http://www.netex.org.uk/netex}DestinationDisplay", DestinationDisplay)
 
@@ -195,6 +329,29 @@ def conversion(input_filename: str, output_filename: str):
         time_demand_types = list(tdts.values())
         timing_links = list(tls.values())
 
+    if len(routes) == 0:
+        ssps = {x.id: x for x in scheduled_stop_points}
+        tps = {x.id: x for x in timing_points}
+        tls = {x.id: x for x in timing_links}
+        sls = {x.id: x for x in service_links}
+        rps = {x.id: x for x in route_points} # TODO: dit werkt niet, want we gebruiken in
+        rls = {x.id: x for x in route_links}  # TODO: de functie niet het id als key
+
+        for sjp in service_journey_patterns:
+            route = routeFromServiceJourneyPattern(sjp, ssps, tps, tls, sls, rps, rls)
+            sjp.route_ref_or_route_view = getRef(route)
+            routes.append(route)
+
+        route_points = list(rps.values())
+        route_links = list(rls.values())
+
+    if len(vehicle_types) == 0:
+        vt = VehicleType(id=getId(VehicleType, codespaces[0], "UNKNOWN"), version=versions[0].version, name=MultilingualString(value="Onbekend"))
+        vehicle_types.append(vt)
+        vt_ref = getRef(vt)
+        for sj in service_journeys:
+            sj.compound_train_ref_or_train_ref_or_vehicle_type_ref = vt_ref
+
     for sj in service_journeys:
         sj.private_code = PrivateCode(type_value="JourneyNumber",
                                       value=str(int(str(sj.departure_time).replace(':', ''))))
@@ -219,6 +376,10 @@ def conversion(input_filename: str, output_filename: str):
     acs = {x.id: x for x in availability_conditions}
     for sj in service_journeys:
         timeZoneConversion(sj, acs, tz_in,tz_out)
+    for operator in operators:
+        operator.locale.time_zone = None
+        operator.locale.summer_time_zone = None
+        operator.locale.time_zone_offset = None
 
     dutchprofile = DutchProfile(codespaces[0], data_sources[0], versions[0])
     resource_frames = dutchprofile.getResourceFrames(data_sources=data_sources, responsibility_sets=responsibility_sets,
@@ -272,7 +433,7 @@ def conversion(input_filename: str, output_filename: str):
     tree.write("netex-output/dutch-filter.xml", pretty_print=True, strip_text=True)
 
 if __name__ == '__main__':
-    for input_filename in glob.glob("netex-output-epip/Flix_Line_811.xml"):
+    for input_filename in glob.glob("netex-output-epip/Flix_Line_025.xml"):
         print(input_filename)
         output_filename = input_filename.replace('netex-output/', 'netex-output-epip/')
         conversion(input_filename, output_filename)
