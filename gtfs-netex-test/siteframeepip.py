ea00e0aef86c515f6f078a55de47f69e748a1b96
Stefan de Konink <stefan@konink.de>
2023-12-13 23:11:03 +0100

    Add some quays

--- gtfs-netex-test/siteframeepip.py
+++ gtfs-netex-test/siteframeepip.py
@@ -1,12 +1,25 @@
 from typing import List
 
 from netex import Codespace, ScheduledStopPoint, StopPlace, SiteFrame, StopPlacesInFrameRelStructure, \
-    PassengerStopAssignment, StopPlaceRef, SimplePointVersionStructure, StopTypeEnumeration
+    PassengerStopAssignment, StopPlaceRef, SimplePointVersionStructure, StopTypeEnumeration, QuaysRelStructure, Quay, \
+    QuayTypeEnumeration, QuayRef
 from refs import getId, getRef
 
 
 class SiteFrameEPIP:
     codespace: Codespace
+    @staticmethod
+    def getQuay(scheduled_stop_point: ScheduledStopPoint):
+        quay = Quay(id=scheduled_stop_point.id.replace('ScheduledStopPoint', 'Quay'),
+                    version=scheduled_stop_point.version,
+                    derived_from_object_ref=scheduled_stop_point.id,
+                    derived_from_version_ref_attribute=scheduled_stop_point.version,
+                    name=scheduled_stop_point.name,
+                    name_suffix=scheduled_stop_point.name_suffix,
+                    alternative_texts=scheduled_stop_point.alternative_texts,
+                    centroid=SimplePointVersionStructure(location=scheduled_stop_point.location),
+                    quay_type=QuayTypeEnumeration.BUS_STOP)
+        return quay
 
     @staticmethod
     def getStopPlace(scheduled_stop_point: ScheduledStopPoint):
@@ -18,7 +31,8 @@ class SiteFrameEPIP:
                                name_suffix=scheduled_stop_point.name_suffix,
                                alternative_texts=scheduled_stop_point.alternative_texts,
                                centroid=SimplePointVersionStructure(location=scheduled_stop_point.location),
-                               stop_place_type=StopTypeEnumeration.BUS_STATION
+                               stop_place_type=StopTypeEnumeration.BUS_STATION,
+                               quays=QuaysRelStructure(taxi_stand_ref_or_quay_ref_or_quay=[SiteFrameEPIP.getQuay(scheduled_stop_point)])
                                )
         return stop_place
     @staticmethod
@@ -32,7 +46,8 @@ class SiteFrameEPIP:
                                 version=scheduled_stop_point.version,
                                 order=1,
                                 fare_scheduled_stop_point_ref_or_scheduled_stop_point_ref_or_scheduled_stop_point=getRef(scheduled_stop_point),
-                                taxi_rank_ref_or_stop_place_ref_or_stop_place=StopPlaceRef(ref=scheduled_stop_point.id.replace('ScheduledStopPoint', 'StopPlace'), version=scheduled_stop_point.version)
+                                taxi_rank_ref_or_stop_place_ref_or_stop_place=StopPlaceRef(ref=scheduled_stop_point.id.replace('ScheduledStopPoint', 'StopPlace'), version=scheduled_stop_point.version),
+                                taxi_stand_ref_or_quay_ref_or_quay=QuayRef(ref=scheduled_stop_point.id.replace('ScheduledStopPoint', 'Quay'), version=scheduled_stop_point.version)
                                 )
     @staticmethod
     def getPassengerStopAssignments(scheduled_stop_points: List[ScheduledStopPoint]):
