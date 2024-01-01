74cb78f3d2a44eaa15ebee7f17bc5fc48a320b23
Stefan de Konink <stefan@konink.de>
2024-01-01 16:14:58 +0100

    Update

---
+++ gtfs-netex-test/netex/vehicle_journey_stop_assignments_in_frame_rel_structure.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import List
+from .containment_aggregation_structure import ContainmentAggregationStructure
+from .vehicle_journey_stop_assignment import VehicleJourneyStopAssignment
+
+
+from typing import ClassVar as RestrictedVar
+
+__NAMESPACE__ = "http://www.netex.org.uk/netex"
+
+
+@dataclass(kw_only=True)
+class VehicleJourneyStopAssignmentsInFrameRelStructure(
+    ContainmentAggregationStructure
+):
+    class Meta:
+        name = "vehicleJourneyStopAssignmentsInFrame_RelStructure"
+
+    vehicle_journey_stop_assignment: List[
+        VehicleJourneyStopAssignment
+    ] = field(
+        default_factory=list,
+        metadata={
+            "name": "VehicleJourneyStopAssignment",
+            "type": "Element",
+            "namespace": "http://www.netex.org.uk/netex",
+            "min_occurs": 1,
+        },
+    )
