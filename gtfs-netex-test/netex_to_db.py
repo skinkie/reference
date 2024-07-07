75ef3011165022a8ee92fdbc1256851b699d957b
Stefan de Konink <stefan@konink.de>
2024-07-07 21:44:03 +0200

    The new any importer.

---
+++ gtfs-netex-test/netex_to_db.py
@@ -0,0 +1,46 @@
+from typing import List
+
+import duckdb as sqlite3
+import os
+
+from isal import igzip_threaded
+
+from anyintodbnew import  get_interesting_classes, setup_database, open_netex_file, insert_database
+
+def open_netex_file(filename):
+    if filename.endswith('.xml.gz'):
+        yield igzip_threaded.open(filename, 'rb', compresslevel=3, threads=3)
+    elif filename.endswith('.xml'):
+        yield open(filename, 'rb')
+    elif filename.endswith('.zip'):
+        import zipfile
+        zip = zipfile.ZipFile(filename)
+        for zipfilename in zip.filelist:
+            yield zip.open(zipfilename)
+
+def main(filenames: List[str], database: str, clean_database: bool = True):
+    # Workaround for https://github.com/duckdb/duckdb/issues/8261
+    try:
+        os.remove(database)
+    except:
+        pass
+
+    with sqlite3.connect(database) as con:
+        classes = get_interesting_classes()
+
+        if clean_database:
+            setup_database(con, classes, clean_database)
+
+        for filename in filenames:
+            for sub_file in open_netex_file(filename):
+                insert_database(con, classes, sub_file)
+
+if __name__ == '__main__':
+    import argparse
+    argument_parser = argparse.ArgumentParser(description='Import any NeTEx source into DuckDB')
+    argument_parser.add_argument('netex', nargs='+', default=[], help='The original DuckDB NeTEx database')
+    argument_parser.add_argument('database', type=str, help='The DuckDB to be overwritten with the NeTEx context')
+    argument_parser.add_argument('clean_database', action="store_true", help='Clean the current file', default=True)
+    args = argument_parser.parse_args()
+
+    main(args.netex, args.database, args.clean_database)
