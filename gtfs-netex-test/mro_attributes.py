eb2e426726f01b2d3d8f7c0a32985f86e9b6c2e5
Stefan de Konink <stefan@konink.de>
2024-07-08 02:12:14 +0200

    Implement the related explorer. Fix #39

---
+++ gtfs-netex-test/mro_attributes.py
@@ -0,0 +1,158 @@
+import dataclasses
+import decimal
+from dataclasses import Field
+
+import xsdata
+from xsdata.models.datatype import XmlDateTime, XmlDuration, XmlTime, XmlDate, XmlPeriod
+
+import netex
+import inspect
+import typing
+
+def hasdefault(default):
+    if isinstance(default, dataclasses._MISSING_TYPE):
+        return None
+
+    return default
+
+def unembed(fields):
+    all_references = []
+    all_classes = []
+
+    for name, field in fields.items():
+        resolved_class = resolve_class(field.type)
+        if not resolved_class:
+            continue
+
+        if hasattr(resolved_class, "__forward_arg__"):
+            continue
+
+        resolved_class_name = resolved_class.__mro__[0].__name__
+
+        if resolved_class_name.endswith('Ref'):
+            all_references.append(resolved_class_name[:-3])
+
+        all_classes.append((name, field, resolved_class_name))
+
+    result = []
+    for name, field, resolved_class_name in all_classes:
+        if resolved_class_name not in all_references:
+            result.append((name, field))
+
+    return result
+
+def resolve_class(clazz):
+    clazz_resolved = clazz
+
+    if hasattr(clazz, '_name'):
+        if clazz._name == 'Optional':
+            optional = True
+            clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
+        elif clazz._name == 'List':
+            return None # TODO: handle list elements
+        else:
+            clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
+
+    return clazz_resolved
+
+IGNORE_ATTRIBUTES = ['name_of_class_attribute']
+def list_attributes(clazz, parent_name=None):
+    buffer = []
+    for name, field in unembed(clazz.__dataclass_fields__):
+        if name in IGNORE_ATTRIBUTES:
+            continue
+
+        if parent_name:
+            full_name = parent_name + '.' + name
+            buffer.append((full_name, get_type(field.type, full_name), hasdefault(field.default), field))
+        else:
+            buffer.append((name, get_type(field.type, name), hasdefault(field.default), field))
+            # return (name, get_type(field.type, name))
+
+    return buffer
+
+def likely_type(obj: Field):
+    if hasattr(obj.type, '__args__'):
+        if hasattr(obj.type.__args__[0], '__name__'):
+            return obj.type.__args__[0].__name__
+
+        elif obj.type.__args__[0].__class__ == typing.ForwardRef:
+            return obj.type.__args__[0].__forward_arg__
+
+        else:
+            return obj.type.__class__.__name__
+    elif obj.type.__class__.__name__ == 'type':
+        return obj.type.__name__
+    else:
+        return obj.type.__class__.__name__
+
+
+def get_type(clazz, parent_name):
+    optional = False
+    clazz_resolved = clazz
+
+    if clazz == typing.List[object]:
+        # We don't handle these yet
+        return None
+
+    if hasattr(clazz, '_name'):
+        if clazz._name == 'Optional':
+            optional = True
+            clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
+        elif clazz._name == 'List':
+            return None # TODO: handle list elements
+        else:
+            clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
+
+    if clazz_resolved == object:
+        # TODO: We don't handle these yet
+        return None
+
+    if hasattr(clazz_resolved, "_name") and clazz_resolved._name == 'List':
+        # TODO: We don't handle these yet
+        return None
+
+    if hasattr(clazz_resolved, "__forward_arg__"):
+        # TODO: We don't handle these yet
+        return None
+
+    if not hasattr(clazz_resolved, "__mro__"):
+         print(parent_name, clazz_resolved)
+         print()
+
+    if len([x for x in clazz_resolved.__mro__ if x.__name__.endswith('RelStructure')]) > 0:
+        return None
+
+    if len([x for x in clazz_resolved.__mro__ if x.__name__ == 'Enum']) > 0:
+        return (str, optional) # (get_enum(clazz_resolved), optional)
+
+    # This is a hack because upstream did not use VersionOfObjectRef as substitution group consistently
+    if len([x for x in clazz_resolved.__mro__ if x.__name__.endswith('RefStructure')]) > 0:
+        # Inline RefClasses
+        # return (list_attributes(clazz_resolved, parent_name), optional)
+        return (netex.VersionOfObjectRef, optional)
+
+    if clazz_resolved not in (int, str, bool, float, bytes, XmlPeriod, XmlTime, XmlDate, XmlDateTime, XmlDuration, decimal.Decimal, netex.MultilingualString):
+        listed_attributes = list_attributes(clazz_resolved, parent_name)
+        if listed_attributes:
+            return (listed_attributes, optional)
+        return None
+
+    return (clazz_resolved, optional)
+
+def get_enum(clazz):
+    return [x[1].value for x in inspect.getmembers(clazz) if isinstance(x[1], clazz)]
+
+
+# members = inspect.getmembers(netex)
+# class_list = [x[1] for x in members]
+
+# print(netex.DataManagedObjectStructure.__dataclass_fields__)
+
+# for name, field in netex.DataManagedObjectStructure.__dataclass_fields__.items():
+#     print(name, get_type(field.type))
+
+# print('...')
+
+# for x in [x for x in netex.DataManagedObjectStructure.__mro__ if x in class_list]:
+#     print(x.__dataclass_fields__)
---
+++ gtfs-netex-test/related_explorer.py
@@ -0,0 +1,88 @@
+from decimal import Decimal
+from typing import List, Tuple
+
+from isal import igzip_threaded
+from xsdata.formats.dataclass.serializers import XmlSerializer
+from xsdata.formats.dataclass.serializers.config import SerializerConfig
+from xsdata.formats.dataclass.serializers.writers import XmlEventWriter
+from xsdata.models.datatype import XmlDateTime, XmlDuration, XmlTime
+
+from dbaccess import load_local
+import netex
+from netex import ServiceJourney, VersionOfObjectRef, MultilingualString, ScheduledStopPointRef, \
+    VersionOfObjectRefStructure, GeneralFrame, PublicationDelivery, ParticipantRef, DataObjectsRelStructure, \
+    GeneralFrameMembersRelStructure
+import duckdb as sqlite3
+from mro_attributes import list_attributes
+import xsdata
+
+serializer_config = SerializerConfig(ignore_default_attributes=True, xml_declaration=True)
+serializer_config.pretty_print = True
+serializer_config.ignore_default_attributes = True
+serializer = XmlSerializer(config=serializer_config, writer=XmlEventWriter)
+
+def recursive_attributes(obj):
+    for k, v in obj.__dict__.items():
+        if v is not None:
+            # print(v)
+            if issubclass(v.__class__, VersionOfObjectRef) or issubclass(v.__class__, VersionOfObjectRefStructure):
+                yield v
+
+            else:
+                if (v.__class__.__name__ in netex.__all__ and hasattr(v, '__dict__')):
+                    yield from recursive_attributes(v)
+                elif v.__class__ in (list, tuple):
+                    for x in v:
+                        yield from recursive_attributes(x)
+
+
+def recursive_resolve(con, parent, resolved):
+    resolved.append(parent)
+
+    for obj in recursive_attributes(parent):
+        if obj.name_of_ref_class is None:
+            # Hack, because NeTEx does not define the default name of ref class yet
+            obj.name_of_ref_class = obj.__class__.__name__[0:-3]
+
+        if obj in resolved:
+            continue
+
+        resolved_obj = load_local(con, getattr(netex, obj.name_of_ref_class), 1)[0]
+        recursive_resolve(con, resolved_obj, resolved)
+
+def fetch(database: str, output_filename: str):
+    with sqlite3.connect(database) as con:
+        service_journeys: List[ServiceJourney] = load_local(con, ServiceJourney, 1)
+        service_journey = service_journeys[0]
+
+        resolved = []
+        recursive_resolve(con, service_journey, resolved)
+
+        publication_delivery = PublicationDelivery(
+            version="ntx:1.1",
+            publication_timestamp=XmlDateTime.now(),
+            participant_ref=ParticipantRef(value="PyNeTExConv"),
+            data_objects=DataObjectsRelStructure(choice=[
+                GeneralFrame(
+                    id="Results", version="1",
+                    members=GeneralFrameMembersRelStructure(choice=resolved))]))
+
+        ns_map = {'': 'http://www.netex.org.uk/netex', 'gml': 'http://www.opengis.net/gml/3.2'}
+
+        if output_filename.endswith('.gz'):
+            with igzip_threaded.open(output_filename, 'wt', compresslevel=3, threads=3, block_size=2*10**8, encoding='utf-8') as out:
+                serializer.write(out, publication_delivery, ns_map)
+        elif output_filename == '-':
+            print(serializer.render(publication_delivery, ns_map))
+        else:
+            with open(output_filename, 'w', encoding='utf-8') as out:
+                serializer.write(out, publication_delivery, ns_map)
+
+if __name__ == '__main__':
+    import argparse
+    argument_parser = argparse.ArgumentParser(description='Export a prepared EPIP  import into DuckDB')
+    argument_parser.add_argument('netex', type=str, help='The original DuckDB NeTEx database')
+    argument_parser.add_argument('output', type=str, nargs="?", default="-", help='The NeTEx output filename, for example: netex.xml.gz')
+    args = argument_parser.parse_args()
+
+    fetch(args.netex, args.output)
