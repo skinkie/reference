83f915958e2ee67cffe4956c8dbe98641ee7dfe9
Stefan de Konink <stefan@konink.de>
2024-12-06 16:00:27 +0100

    Fix import

--- gtfs-netex-test/netex_to_db.py
+++ gtfs-netex-test/netex_to_db.py
@@ -8,6 +8,7 @@ from netexio.dbaccess import get_interesting_classes, setup_database, open_netex
     resolve_all_references_and_embeddings
 from netexio.xmlserializer import MyXmlSerializer
 from aux_logging import *
+from transformers.embedding import embedding_update
 
 
 def main(filenames: List[str], database: str, clean_database: bool = True, referencing: bool = False):
@@ -29,7 +30,7 @@ def main(filenames: List[str], database: str, clean_database: bool = True, refer
                 insert_database(db, classes, sub_file)
 
         if referencing:
-            resolve_all_references_and_embeddings(db, classes)
+            embedding_update(db)
 
 if __name__ == '__main__':
     import argparse
@@ -37,13 +38,10 @@ if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser(description='Import any NeTEx source into DuckDB')
     argument_parser.add_argument('netex', nargs='+', default=[], help='NeTEx files')
     argument_parser.add_argument('database', type=str, help='The DuckDB to be overwritten with the NeTEx context')
-    argument_parser.add_argument('clean_database', action="store_true", help='Clean the current file', default=True)
-    argument_parser.add_argument('clean_database', action="store_true", help='Clean the current file')
-    argument_parser.add_argument('referencing', action="store_false", help='Create referencing table')
+    argument_parser.add_argument('--clean_database', action="store_true", help='Clean the current file', default=True)
+    argument_parser.add_argument('--referencing', action="store_false", help='Create referencing table')
     argument_parser.add_argument('--log_file', type=str, required=False, help='the logfile')
     args = argument_parser.parse_args()
-    mylogger =prepare_logger(logging.INFO,args.log_file)
+    mylogger = prepare_logger(logging.INFO,args.log_file)
 
-
-    # main(args.netex, args.database, args.clean_database, args.referencing)
-    main(args.netex, args.database, False, args.referencing)
+    main(args.netex, args.database, args.clean_database, args.referencing)
