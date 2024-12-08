ffcd389f8f9b13520d23900a54fae5754405fa42
Stefan de Konink <stefan@konink.de>
2024-12-08 11:51:39 +0100

    Implement that any object can be reversioned.
    But retain the ability to keep 'any'
    Just to mess with somGermany companies.
    
    Fix #157

--- gtfs-netex-test/test_reversion.py
+++ gtfs-netex-test/test_reversion.py
@@ -1,7 +1,8 @@
-import duckdb
-
 from netexio.database import Database
 from transformers.reversion import reversion_all_objects
 
-with Database("/tmp/authorities.duckdb", read_only=False) as db:
+with Database("/tmp/authorities-any.duckdb", read_only=False) as db:
     reversion_all_objects(db, "20241114")
+
+with Database("/tmp/authorities.duckdb", read_only=True) as db:
+    reversion_all_objects(db, "20241114", True)
--- gtfs-netex-test/transformers/reversion.py
+++ gtfs-netex-test/transformers/reversion.py
@@ -29,32 +29,42 @@ def simple_recursive_attributes(obj):
                                 yield from simple_recursive_attributes(x)
 
 
-def reversion_object(deserialized, updated_version):
+def reversion_object(deserialized, updated_version, any_too=False):
     for obj in simple_recursive_attributes(deserialized):
-        if hasattr(obj, 'version') and obj.version != 'any':
+        if hasattr(obj, 'version') and (any_too or obj.version != 'any'):
             obj.version = updated_version
 
-    if hasattr(object, 'version') and object.version != 'any':
+    if hasattr(object, 'version') and (any_too or object.version != 'any'):
         object.version = updated_version
 
 
-def reversion_udf(serializer: Serializer, serialized: bytes, clazz: str, updated_version: str) -> bytes:
+def reversion_udf(serializer: Serializer, serialized: bytes, clazz: str, updated_version: str, any_too: bool = False) -> bytes:
     deserialized = serializer.unmarshall(serialized, clazz)
-    reversion_object(deserialized, updated_version)
+    reversion_object(deserialized, updated_version, any_too)
     return serializer.marshall(deserialized, clazz)
 
 
-def reversion_all_objects(db: Database, updated_version: str):
+def reversion_all_objects(db: Database, updated_version: str, any_too: bool = False):
     con = db.con
     con.create_function('reversion', functools.partial(reversion_udf, db.serializer))
 
-    for objectname in db.serializer.clean_element_names:
-        con.execute(f"UPDATE {objectname} SET version = ?, object = reversion(object, '{objectname}', ?);",
-                    (updated_version, updated_version,))
+    for objectname in db.tables():
+        if any_too:
+            con.execute(f"UPDATE {objectname} SET version = ?, object = reversion(object, '{objectname}', ?, ?);",
+                        (updated_version, updated_version, any_too))
+        else:
+            con.execute(f"UPDATE {objectname} SET version = CASE WHEN version == 'any' THEN version ELSE ? END, object = reversion(object, '{objectname}', ?, ?);",
+                        (updated_version, updated_version, any_too))
 
     con.remove_function('reversion')
 
-    con.execute("UPDATE embedded SET parent_version = ? WHERE parent_version <> 'any';", (updated_version,))
-    con.execute("UPDATE embedded SET version = ? WHERE version <> 'any';", (updated_version,))
-    con.execute("UPDATE referencing SET parent_version = ? WHERE parent_version <> 'any';", (updated_version,))
-    con.execute("UPDATE referencing SET version = ? WHERE version <> 'any';", (updated_version,))
+    if any_too:
+        con.execute("UPDATE embedded SET parent_version = ?;", (updated_version,))
+        con.execute("UPDATE embedded SET version = ?;", (updated_version,))
+        con.execute("UPDATE referencing SET parent_version = ?;", (updated_version,))
+        con.execute("UPDATE referencing SET version = ?;", (updated_version,))
+    else:
+        con.execute("UPDATE embedded SET parent_version = ? WHERE parent_version <> 'any';", (updated_version,))
+        con.execute("UPDATE embedded SET version = ? WHERE version <> 'any';", (updated_version,))
+        con.execute("UPDATE referencing SET parent_version = ? WHERE parent_version <> 'any';", (updated_version,))
+        con.execute("UPDATE referencing SET version = ? WHERE version <> 'any';", (updated_version,))
