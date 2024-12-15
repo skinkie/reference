ad88158fb06f1aa29f84f9dd5ef8020d8c1e4dfb
Matthias Günter <matthias.guenter@sbb.ch>
2024-12-15 22:03:38 +0100

    fixes on master (#201)
    * stuff I changed
    
    * Apply suggestions from code review
    
    * Do not use ATTACH on Windows
    
    ---------
    
    Co-authored-by: Stefan de Konink <stefan@konink.de>

--- gtfs-netex-test/netexio/dbaccess.py
+++ gtfs-netex-test/netexio/dbaccess.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from typing import T, List, Generator, Tuple
 
 import duckdb
@@ -585,8 +586,9 @@ def write_generator(db: Database, clazz, generator: Generator, empty=False):
     print('\n')
 
 def copy_table(db_read: Database, db_write: Database, classes: list, clean=False):
-    if db_read.read_only:
-        db_write.con.execute(f"ATTACH IF NOT EXISTS '{db_read.database_file}' AS db_read (READ_ONLY);")
+    if db_read.read_only and os.name != 'nt':
+        print (f"ATTACH IF NOT EXISTS '{db_read.database_file}' AS db_read (READ_ONLY);")
+        db_write.con.execute(f"ATTACH DATABASE '{db_read.database_file}' AS db_read (READ_ONLY);")
         for clazz in classes:
             objectname = get_object_name(clazz)
             try:
