ccaa21eeb9d0301be5ed20e93b69ba27db24ca52
Stefan de Konink <stefan@konink.de>
2024-12-16 01:19:19 +0100

    Test DuckDB attach

---
+++ gtfs-netex-test/validate-attach.py
@@ -0,0 +1,9 @@
+import duckdb
+
+with duckdb.connect("/tmp/ro.duckdb", read_only=False) as ro:
+    ro.execute("CREATE TABLE test AS SELECT 1 AS test;")
+
+with duckdb.connect("/tmp/rw.duckdb") as rw:
+    with duckdb.connect("/tmp/ro.duckdb", read_only=True) as ro:
+        rw.execute("ATTACH IF NOT EXISTS '/tmp/ro.duckdb' AS db_read (READ_ONLY);")
+        rw.execute("CREATE TABLE test2 AS SELECT * FROM db_read.test;")
