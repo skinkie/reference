aaf8a6fb67c6ae527f5525204846d2491ca172d2
Stefan de Konink <stefan@konink.de>
2024-12-17 19:29:42 +0100

    test windows again

--- gtfs-netex-test/validate-attach.py
+++ gtfs-netex-test/validate-attach.py
@@ -3,6 +3,8 @@ import duckdb
 ro_file = 'ro.duckdb'
 rw_file = 'rw.duckdb'
 
+duckdb.connect(ro_file, read_only=False)
+
 class Database:
     database_file: str
     read_only: bool
@@ -23,25 +25,17 @@ class Database:
     def __exit__(self, exception_type, exception_value, exception_traceback):
         self.con.close()
 
-def copy_table(db_read: Database, db_write: Database, classes: list, clean=False):
-    sql = f"ATTACH DATABASE '{db_read.database_file}' AS db_read (READ_ONLY);"
-    print(sql)
-    db_write.con.execute(sql)
-    db_write.con.execute(f"DETACH db_read;")
 
 """
+with duckdb.connect(rw_file, read_only=False) as target_db:
+    with duckdb.connect(ro_file, read_only=True) as source_db:
+        target_db.execute(f"ATTACH DATABASE '{ro_file}' AS db_read (READ_ONLY);")
+        target_db.execute(f"DETACH db_read;")
+
+        print(source_db)
+"""
+
 with Database(rw_file, read_only=False) as target_db:
     with Database(ro_file, read_only=True) as source_db:
-        copy_table(source_db, target_db, ['test'])
-"""
-
-with duckdb.connect(rw_file, read_only=False) as target_db:
-    cur_rw = target_db.cursor()
-
-    with duckdb.connect(ro_file, read_only=True) as source_db:
-        cur_ro = source_db.cursor()
-
-        cur_rw.execute(f"ATTACH DATABASE '{ro_file}' AS db_read (READ_ONLY);")
-        cur_rw.execute(f"DETACH db_read;")
-
-        print(cur_ro)
+        target_db.con.execute(f"ATTACH DATABASE '{ro_file}' AS db_read (READ_ONLY);")   
+        target_db.con.execute(f"DETACH db_read;")
