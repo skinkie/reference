1f89aa276622b1ebc6a0b244ab30f064b1e50a5a
Matthias Günter <matthias.guenter@sbb.ch>
2025-02-14 16:48:22 +0100

    write xml also into zip

--- gtfs-netex-test/netexio/xml.py
+++ gtfs-netex-test/netexio/xml.py
@@ -2,7 +2,8 @@ from isal import igzip_threaded
 from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
 from xsdata.formats.dataclass.serializers.writers import XmlEventWriter
-
+import zipfile
+import io
 from netex import PublicationDelivery
 
 def export_publication_delivery_xml(publication_delivery: PublicationDelivery, output_filename: str):
@@ -17,6 +18,19 @@ def export_publication_delivery_xml(publication_delivery: PublicationDelivery, o
         with igzip_threaded.open(output_filename, 'wt', compresslevel=3, threads=3, block_size=2 * 10 ** 8,
                                  encoding='utf-8') as out:
             serializer.write(out, publication_delivery, ns_map)
+    elif output_filename.endswith('.zip'):
+        # Create an in-memory text stream
+        xml_buffer = io.StringIO()
+        serializer.write(xml_buffer, publication_delivery, ns_map)
+
+        # Get the XML content as a string
+        xml_content = xml_buffer.getvalue()
+
+        # Write the XML content to a zip file
+        with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            # Use a BytesIO buffer to write the string as bytes
+            with io.BytesIO(xml_content.encode('utf-8')) as xml_bytes:
+                zipf.writestr('publication_delivery.xml', xml_bytes.read())
     else:
         with open(output_filename, 'w', encoding='utf-8') as out:
             serializer.write(out, publication_delivery, ns_map)
