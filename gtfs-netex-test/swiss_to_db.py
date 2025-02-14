43194b9f46f939630e2facbdae4b56347b8bfa65
Stefan de Konink <stefan@konink.de>
2025-02-14 18:25:39 +0100

    Some extra logging messages

--- gtfs-netex-test/swiss_to_db.py
+++ gtfs-netex-test/swiss_to_db.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import os
@@ -32,10 +33,13 @@ def main(swiss_zip_file: str, database: str, clean_database: bool = True, refere
 
         setup_database(db, classes, clean_database)
 
+        log_once(logging.INFO, f"Starting to load {swiss_zip_file}")
         for file in open_netex_file(swiss_zip_file):
+            log_once(logging.INFO, f"Inserting {file}")
             insert_database(db, classes, file)
 
         if referencing:
+            log_once(logging.INFO, f"Embedding update!")
             embedding_update(db)
 
 def check_if_swiss_file(file_handler):
